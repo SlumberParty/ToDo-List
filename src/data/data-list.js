98c41bac58796d9233270a8efd2aefb93d6a7602
Claire Cassidy <clacassidy@gmail.com>
2019-05-20 16:35:41 -0700

    created static HTML, css styling and data

---
+++ src/data/data-list.js
@@ -0,0 +1,20 @@
+const todos = [
+    {
+        task: 'Learn templates',
+        completed: true
+    },
+    {
+        task: 'Learn forEach',
+        completed: false
+    },
+    {
+        task: 'Learn to Breathe',
+        completed: true
+    },
+    {
+        task: 'Learn to code',
+        completed: true
+    }
+];
+
+export default todos;
